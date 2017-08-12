c322d83e388cefa45dbbd52287a38182e883186b
Raz1789 <navaneeth_3@hotmail.com>
2017-08-12 23:14:18 +0400

    Inital Commit
    My Parallel Programming with Performance analysis.

---
+++ src/main/scala/scalashop/VerticalBoxBlur.scala
@@ -0,0 +1,80 @@
+package scalashop
+
+import org.scalameter._
+import common._
+
+object VerticalBoxBlurRunner {
+
+  val standardConfig = config(
+    Key.exec.minWarmupRuns -> 5,
+    Key.exec.maxWarmupRuns -> 10,
+    Key.exec.benchRuns -> 10,
+    Key.verbose -> true
+  ) withWarmer(new Warmer.Default)
+
+  def main(args: Array[String]): Unit = {
+    val radius = 3
+    val width = 1920
+    val height = 1080
+    val src = new Img(width, height)
+    val dst = new Img(width, height)
+    val seqtime = standardConfig measure {
+      VerticalBoxBlur.blur(src, dst, 0, width, radius)
+    }
+    println(s"sequential blur time: $seqtime ms")
+
+    val numTasks = 32
+    val partime = standardConfig measure {
+      VerticalBoxBlur.parBlur(src, dst, numTasks, radius)
+    }
+    println(s"fork/join blur time: $partime ms")
+    println(s"speedup: ${seqtime / partime}")
+  }
+
+}
+
+/** A simple, trivially parallelizable computation. */
+object VerticalBoxBlur {
+
+  /** Blurs the columns of the source image `src` into the destination image
+   *  `dst`, starting with `from` and ending with `end` (non-inclusive).
+   *
+   *  Within each column, `blur` traverses the pixels by going from top to
+   *  bottom.
+   */
+  def blur(src: Img, dst: Img, from: Int, end: Int, radius: Int): Unit = {
+    // TODO implement this method using the `boxBlurKernel` method
+    var xIter: Int = from
+    var yIter: Int =0
+    while(xIter < end){
+      yIter = 0
+      while(yIter < src.height ){
+        dst(xIter,yIter) = boxBlurKernel(src,xIter,yIter,radius)
+        yIter+=1
+      }
+      xIter+=1
+    }
+  }
+
+  /** Blurs the columns of the source image in parallel using `numTasks` tasks.
+   *
+   *  Parallelization is done by stripping the source image `src` into
+   *  `numTasks` separate strips, where each strip is composed of some number of
+   *  columns.
+   */
+  def parBlur(src: Img, dst: Img, numTasks: Int, radius: Int): Unit = {
+    // TODO implement using the `task` construct and the `blur` method
+    var strip: Int =0
+    if(numTasks <= src.width)
+      strip = src.width / numTasks
+    else
+      strip = 1
+
+    val splitPoint = 0 to src.width by strip
+    val splitPair = splitPoint zip splitPoint.tail
+    val tasks = splitPair map { case(start,end) => task(blur(src,dst,start,end,radius))}
+
+    tasks.foreach(x => x.join())
+  }
+
+}
